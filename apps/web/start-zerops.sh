5d5b415b784b1f3f634f3059f73bd562a5321cf2
Thirumurugan-12 <a.thirumurugan13943@gmail.com>
2026-08-08 19:43:13 +0530

    Run native web service from production Next bundle

--- apps/web/start-zerops.sh
+++ apps/web/start-zerops.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -eu
 cd /var/www/apps/web
-exec /var/www/apps/web/node_modules/.bin/next dev --hostname 0.0.0.0 --port 3000
+exec /var/www/apps/web/node_modules/.bin/next start --hostname 0.0.0.0 --port 3000
